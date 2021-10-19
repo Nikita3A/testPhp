1a6ad2540c14b5dd329ec9f6a34d67e55eeb1684
Nikita3A <87770124+Nikita3A@users.noreply.github.com>
2021-10-19 19:32:37 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Css -->
+    <link rel="stylesheet" href="app.css">
+    <!-- Bootstrap CDN Css -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+    <title>Birthdays</title>
+  </head>
+  <body>
